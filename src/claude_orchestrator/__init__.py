919015e213f5ce34d154545d3cc84c593513c984
Javier <javier@simplekyc.com>
2025-12-19 17:59:19 +0100

    fix: formatting

--- src/claude_orchestrator/__init__.py
+++ src/claude_orchestrator/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.4.1"
 
 from claude_orchestrator.config import Config, ReviewConfig, load_config
 from claude_orchestrator.git_provider import GitProvider, GitProviderStatus, get_provider_status
-from claude_orchestrator.mcp_registry import AuthType, MCPDefinition, MCP_REGISTRY
+from claude_orchestrator.mcp_registry import MCP_REGISTRY, AuthType, MCPDefinition
 from claude_orchestrator.reviewer import PRInfo, ReviewResult, review_prs_sync
 
 __all__ = [
@@ -22,4 +22,3 @@ __all__ = [
     "ReviewResult",
     "review_prs_sync",
 ]
-
